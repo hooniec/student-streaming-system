a62b14452d6471664bcc9ff25d4ae9bff45b70db
EthanRiwaka <ethanriwaka1@gmail.com>
2022-08-02 12:45:03 +1200

    Add project files.

---
+++ StreamTec/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StreamTec.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
