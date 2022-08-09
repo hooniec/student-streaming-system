b698f8795b542adc6157e6c26d61ed5d7931fd0f
EthanRiwaka <ethanriwaka1@gmail.com>
2022-08-09 16:31:27 +1200

    Created Database Migrations for Stream Table. Also Creates Student table. Auto implements stream data. Relationship Table Added.

---
+++ StreamTec/Models/Stream.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StreamTec.Models
+{
+    public class Stream
+    {
+        //no values in this model class should be null
+        //Data annotations for data filtering
+        [StringLength(50)]
+        public string StreamID { get; set; }
+        [StringLength(50)]
+        public string Room { get; set; }
+        public int Credits { get; set; }
+        [StringLength(50)]
+        public string Day { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public int Capacity { get; set; }
+    }
+}
---
+++ StreamTec/Models/WelTecContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StreamTec.Models
+{
+    public class WelTecContext : DbContext
+    {
+
+        //Creating a constructor for the Context Class inheriting from the base class
+        public WelTecContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        //Creating an object of each model class using the DbSet from EntityFrameworkCore
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Stream> Streams { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.EnableSensitiveDataLogging();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Using the modelBuilder from EntityFrameworkCore we can create an entity of student or stream
+            //and convert it into a table using ToTable();
+            modelBuilder.Entity<Student>().ToTable("Student");
+            modelBuilder.Entity<Stream>().ToTable("Stream");
+        }
+    }
+}
