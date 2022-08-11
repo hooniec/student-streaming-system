f932511426b028330616d124efa3e38a1416eed4
Sunghoon Cho <sunghoon.cho@soit.local>
2022-08-11 14:58:09 +1200

    Created login and registration with session

--- StreamTec/Controllers/HomeController.cs
+++ StreamTec/Controllers/HomeController.cs
@@ -1,16 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using StreamTec.Models;
+using System.Data.Entity.Infrastructure;
 using System.Diagnostics;
 
 namespace StreamTec.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly WelTecContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(WelTecContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -18,8 +19,68 @@ namespace StreamTec.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Timetable()
         {
+            return View(_context.Streams.ToList());
+        }
+
+        // Register Action for registring a student
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register([Bind("StudentId,Email")] Student student)
+        {
+            try
+            {
+                // Add a student details to database and redirect user to homepage
+                if (ModelState.IsValid)
+                {
+                    _context.Add(student);
+                    await _context.SaveChangesAsync();
+                    TempData["message"] = string.Format("Successfully Registered with Student ID: {0}", student.StudentId);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            catch (DbUpdateException /* ex */)
+            {
+                //Log the error (uncomment ex variable name and write a log.
+                ModelState.AddModelError("", "Unable to save changes. " +
+                    "Try again, and if the problem persists " +
+                    "see your system administrator.");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Student student)
+        {
+            try
+            {
+                // Validate a student details
+                if (ModelState.IsValid)
+                {
+                    using (_context)
+                    {
+                        var obj = _context.Students.Where(s => s.StudentId.Equals(student.StudentId) && s.Email.Equals(student.Email)).FirstOrDefault();
+                        if (obj != null)
+                        {
+                            // Add a student details to session
+                            HttpContext.Session.SetString("_StudentId", obj.StudentId.ToString());
+                            HttpContext.Session.SetString("_Email", obj.Email.ToString());
+                            return RedirectToAction("Timetable", "Home");
+                        }
+                    }
+                }
+                TempData["message"] = "Invalid student detilas";
+                return RedirectToAction("Index", "Home");
+            }
+            catch (DbUpdateException /* ex */)
+            {
+                //Log the error (uncomment ex variable name and write a log.
+                ModelState.AddModelError("", "Unable to save changes. " +
+                    "Try again, and if the problem persists " +
+                    "see your system administrator.");
+            }
             return View();
         }
 
--- StreamTec/Program.cs
+++ StreamTec/Program.cs
@@ -7,6 +7,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 // Add services to the DatabaseContext
 builder.Services.AddDbContext<WelTecContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("myconn")));
+// Add services to the session
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(120);
+});
+// Add services to HttpContextAccessor
+builder.Services.AddHttpContextAccessor();
+
 // Build the web application
 var app = builder.Build();
 
@@ -39,7 +48,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-
+app.UseSession();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
