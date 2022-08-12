4fabffc33c669d9ca9ef39a0e6598e800cc8199a
EthanRiwaka <riwaka.ethan@gmail.com>
2022-08-12 16:34:31 +1200

    Adding Enrollment Model for EFCore relationships to work. Functions to display admin data into a table on the AdminHome.cshtml.

---
+++ StreamTec/Models/Enrollment.cs
@@ -0,0 +1,9 @@
+﻿namespace StreamTec.Models
+{
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public List<Stream> StreamF { get; set; }
+        public List<Student> StudentF { get; set; }
+    }
+}
--- StreamTec/Models/WelTecContext.cs
+++ StreamTec/Models/WelTecContext.cs
@@ -4,10 +4,6 @@ namespace StreamTec.Models
 {
     public class WelTecContext : DbContext
     {
-        public WelTecContext()
-        {
-        }
-
         //Creating a constructor for the Context Class inheriting from the base class
         public WelTecContext(DbContextOptions options) : base(options)
         {
@@ -17,6 +13,7 @@ namespace StreamTec.Models
         //Creating an object of each model class using the DbSet from EntityFrameworkCore
         public DbSet<Student> Students { get; set; }
         public DbSet<Stream> Streams { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -27,8 +24,9 @@ namespace StreamTec.Models
         {
             //Using the modelBuilder from EntityFrameworkCore we can create an entity of student or stream
             //and convert it into a table using ToTable();
-            modelBuilder.Entity<Student>().ToTable("Student");
+            modelBuilder.Entity<Student>().ToTable("Student").HasOne(s => Enrollments);
             modelBuilder.Entity<Stream>().ToTable("Stream");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment").HasMany(s => Students).WithOne().HasForeignKey(s => s.StudentId);
         }
     }
 }
