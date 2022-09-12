cd8ba5caa46c190b91ad79eecc4fd3144bfeeea9
EthanRiwaka <riwaka.ethan@gmail.com>
2022-09-12 12:41:30 +1200

    Basic Confirmation for delete and validation for AdminAdd. Merging AdminAdd into AdminHome.

--- StreamTec/Models/Enrollment.cs
+++ StreamTec/Models/Enrollment.cs
@@ -10,8 +10,11 @@ namespace StreamTec.Models
         public virtual Student Students { get; set; }
         public virtual Stream Streams { get; set; }
 
-
+        [RegularExpression(@"^[0-9]*$", ErrorMessage = "Invalid Student ID")]
+        [MinLength(7, ErrorMessage = "Must be 7 Characters long")]
+        [MaxLength(7, ErrorMessage = "Must be 7 Characters long")]
         public string StudentId { get; set; }
+
         public string StreamID { get; set; }
     }
 }
