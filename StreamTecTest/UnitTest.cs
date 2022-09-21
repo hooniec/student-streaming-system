55d40b7076a47d38e4dbc16942a78ee9194d8c2d
Ethan <ethan@DESKTOP-9Q11L94>
2022-09-22 00:10:49 +1200

    Potentially discovered the source of the problem for unit testing

--- StreamTecTest/UnitTest.cs
+++ StreamTecTest/UnitTest.cs
@@ -9,6 +9,7 @@ namespace StreamTecTest
     [TestClass]
     public class UnitTest
     {
+        //https://stackoverflow.com/questions/8818207/how-should-one-unit-test-a-net-mvc-controller
         string searchID = "2208266";
 
         public WelTecContext _context;
@@ -56,6 +57,7 @@ namespace StreamTecTest
             Assert.IsTrue(test.IsCompleted);
         }
 
+        //Do i create a mock?
         [TestMethod]
         public void RegisterTest()
         {
@@ -63,12 +65,13 @@ namespace StreamTecTest
 
             var controller = new HomeController(_context);
             var result = controller.Register(student);
+            Console.WriteLine(result.Exception);
 
             //Assert.AreEqual(result, student.StudentId);
             //Assert.AreEqual(result.Email, "register@email.com");            
 
             Assert.IsTrue(result.IsCompleted);
-
+            Assert.IsNotNull(result);
         }
 
         [TestMethod]
@@ -80,12 +83,10 @@ namespace StreamTecTest
             var result = controller.Index(student);
             var index = controller.Index() as ViewResult;
 
-            //var okResult = result as IActionResult;
-            //Assert.IsNotNull(okResult);
-            //Console.WriteLine(okResult);
-
-            Assert.IsTrue(result.IsCompleted);            
+            Console.WriteLine(result);
+            Assert.IsNotNull(result);
             Assert.IsTrue(index.ViewName == "Index");
+            Assert.IsInstanceOfType(result, typeof(Task<IActionResult>));
         }
 
         [TestMethod]
@@ -95,12 +96,28 @@ namespace StreamTecTest
 
             var controller = new HomeController(_context);
             var result = controller.Index(student);
-
+            Console.Write(result);
             var index = controller.Index() as ViewResult;
+            //Assert.AreEqual(HttpStatusCode.OK, result.Exception);
+            Console.WriteLine(result.Exception);
+           
 
-            Assert.IsFalse(result.IsCompletedSuccessfully);
-            Console.WriteLine(student.StudentId + student.Email);
+            Console.Write(index);
+            Assert.IsTrue(result.IsCompleted);
             Assert.IsTrue(index.ViewName == "Index");
+        }
+        [TestMethod]
+        public void ValidLogoutTest()
+        {
+            var student = new Student { Email = "ethan@email.com", StudentId = "2208266" };
+
+            var controller = new HomeController(_context);
+            var result = controller.Index(student);
+            var logout = controller.Logout();
+
+            Assert.IsTrue(result.IsCompleted);
+            Assert.IsTrue(logout.IsCompleted);
+            Console.WriteLine(logout);
         }
     }
 }
